2afc786ec92d81052fc41ec4d3891d9712f9aa0c
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-04-24 15:39:53 +0530

    project

--- mysql.sh
+++ mysql.sh
@@ -7,23 +7,23 @@ check_root
 echo " please enter DB password"
 read -s mysql_root_password
 dnf install mysql-server -y &>>$LOGFILE
-validate $? "installing mysql server"
+# validate $? "installing mysql server"
 
 systemctl enable mysqld &>>$LOGFILE
-validate $? "Enabling mysql server"
+# validate $? "Enabling mysql server"
 
 systemctl start mysqld &>>$LOGFILE
-validate $? "starting mysql"
+# validate $? "starting mysql"
 
 # mysql_secure_installation --set-root-password ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
 # below code is to make script idempotent 
 
-mysql -h 172.31.87.240 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.20.107 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    validate $? " setting root password "
+    # validate $? " setting root password "
 else
     echo -e " root password is already set up : $G skipping $N "
 fi
